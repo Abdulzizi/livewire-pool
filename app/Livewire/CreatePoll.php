40e5b15342ae4e03dcca62bef8fa59f3e3503325
Abdul Jawad Azizi <jawadabdul307@gmail.com>
2025-08-14 13:32:59 +0700

    add & try livewire action

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,9 +7,15 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = ['Initial Option', 'Second Option'];
 
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function handleClick()
+    {
+        $this->options[] = 'New Option ' . (count($this->options) + 1);
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,8 +1,22 @@
 <div>
-    <h1>Poll Title</h1>
+    <div class="mb-4">
+        <h1 class="text-xl font-bold tracking-wide mb-4">Poll Title</h1>
 
-    <input type="text" wire:model.live="title" placeholder="Enter poll question">
+        <input type="text" wire:model.live="title" placeholder="Enter poll question">
+    </div>
+    {{-- <p>Current Title : {{ $title }}</p> --}}
 
-    <p>Current Title : {{ $title }}</p>
+    <div class="my-4">
+        <button class="btn" wire:click.prevent="handleClick">Add Option</button>
+    </div>
+
+    <div>
+        <h2 class="text-xl font-bold tracking-wide mb-4">Option</h2>
+        @foreach ( $options as $index => $option )
+            <div>
+                {{ $index + 1 }} - {{ $option }}
+            </div>
+        @endforeach
+    </div>
 </div>
 
